cb3d97499c5eec0a7f20e0a223adb8f6e27f27b9
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-04-19 12:14:51 +0100

    feat: added router to reroute page onto subfolder onClick

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import Layout from "../layout/layout";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { Folder } from "./utils/types";
 import filter from "./utils/sort";
 import LoadingSpinner from "@/components/loadingSpinner";
-
-export default function () {
-  return (
-    <Layout>
-      <Home />
-    </Layout>
-  );
-}
+import { FaFolder } from "react-icons/fa";
+import Subfolder from "./subfolder";
 
 function Home() {
   const [filesSystem, setFileSystem] = useState<Folder[]>([]);
+  const router = useRouter();
 
   //fetch data from backend
   async function fetchFileSystem() {
@@ -40,14 +36,41 @@ function Home() {
     };
   }, []);
 
-  console.log(filesSystem, "files");
+  //handle click on folder
+  function handleClick(id: string) {
+    router.push("/subfolder");
+    // return <Subfolder id
+  }
+
   if (filesSystem.length === 0) {
     return <LoadingSpinner />;
   } else {
     return (
-      <div>
-        <div>root</div>
+      <div className="grid grid-cols-5 gap-5">
+        {filesSystem.map((i) => {
+          return (
+            <div
+              className="grid grid-rows-2 w-[100px] justify-center items-center"
+              key={i.id}
+            >
+              <FaFolder
+                size="100px"
+                color="skyblue"
+                onClick={() => handleClick(i.id)}
+              />
+              <div>{i.name}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
+}
+
+export default function () {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
---
+++ src/pages/subfolder.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/layout/layout";
+
+function Subfolder() {
+  return <>subfolder</>;
+}
+
+export default function () {
+  return (
+    <Layout>
+      <Subfolder />
+    </Layout>
+  );
+}
