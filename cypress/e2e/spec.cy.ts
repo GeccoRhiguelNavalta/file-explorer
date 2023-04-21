d6e9e0695525d136fbb12a3a0b23e104bfe71d09
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-04-21 10:09:48 +0100

    feat: cypress testing and cirlce ci connected

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,19 @@
+import type {} from "cypress";
+
+describe("renders the page correctly", () => {
+  it("passes", () => {
+    cy.visit("http://127.0.0.1:3000");
+  });
+});
+
+describe("should show elements on window", () => {
+  it("successfully render h1", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("h1").should("exist");
+  });
+
+  it("successfully renders folders", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("svg").should("exist");
+  });
+});
